60573726c48dcfe7523edec51f8487278a2eb7e9
khalidbelk <khalid.belkassmi-el-hafi@epitech.eu>
2024-11-04 12:47:58 +0100

    feat: add Javadoc documentation for StorageManager methods

--- src/data/StorageManager.java
+++ src/data/StorageManager.java
@@ -12,6 +12,13 @@ import java.io.ObjectOutputStream;
 public class StorageManager {
     private static String Filename = "data.dat";
 
+
+    /**
+     * Saves the products to a file using an ObjectOutputStream.
+     *
+     * @param products The ArrayList of products to save.
+     *
+     */
     public void saveProducts(ArrayList<ClothingItem> products) {
         try {
             ObjectOutputStream outputStream = new ObjectOutputStream(new FileOutputStream(Filename));
@@ -22,6 +29,13 @@ public class StorageManager {
         }
     }
 
+    /**
+     * Loads the products from a file using an ObjectInputStream.
+     *
+     * @return An ArrayList of products read by the ObjectInputStream or an empty list
+     * if the file doesn't exist.
+     *
+     */
     public ArrayList<ClothingItem> loadProducts() {
         File file = new File(Filename);
 
